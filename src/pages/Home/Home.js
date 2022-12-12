e37cb9be748f45c448c0b9a51b33e95e1dbf8c44
Vikasyadav25 <vikassinghyadav6@gmail.com>
2022-12-12 19:42:37 +0530

    Initial commit

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,9 @@
+export default function Home(props) {
+    return <div style={{"font-size":"40px", color:"red", }}>Welcome in Job portal created by Vikas Yadav!! 
+    <br/>
+    Let Go !! Click on Job list</div>;
+}
+
+
+
+
